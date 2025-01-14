73d6d10c69b940142d8a52b37843a510c2b5b25d
Aman Jaiswar <amanjaiswar77777@gmail.com>
2025-01-14 12:50:14 +0530

    cors changes

--- Backend/index.js
+++ Backend/index.js
@@ -19,10 +19,12 @@ app.use(express.json());
 
 app.use(cookieParser())
 
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true
-}))
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 
 app.use(express.json({limit : "10mb"}))
